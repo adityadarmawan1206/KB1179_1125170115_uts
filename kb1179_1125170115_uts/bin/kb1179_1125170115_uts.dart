fb5f83988d68183a22a71bdc65f4bc8534c58048
adityadarmawan12 <hqolbi77@gmail.com>
2025-10-05 11:15:53 +0700

    kb1179_1125170115_uts

--- kb1179_1125170115_uts/bin/kb1179_1125170115_uts.dart
+++
@@ -1,5 +0,0 @@
-import 'package:kb1179_1125170115_uts/kb1179_1125170115_uts.dart' as kb1179_1125170115_uts;
-
-void main(List<String> arguments) {
-  print('Hello world: ${kb1179_1125170115_uts.calculate()}!');
-}
---
+++ kb1179_1125170115_uts/bin/mahasiswa extend asdos.dart
@@ -0,0 +1,52 @@
+// Kelas induk
+class Mahasiswa {
+  // Atribut
+  String nama;
+  String nim;
+  String jurusan;
+  int angkatan;
+
+  
+  Mahasiswa(this.nama, this.nim, this.jurusan, this.angkatan);
+
+  
+  void tampilkanData() {
+    print('=== Data Mahasiswa ===');
+    print('Nama     : $nama');
+    print('NIM      : $nim');
+    print('Jurusan  : $jurusan');
+    print('Angkatan : $angkatan');
+  }
+}
+
+
+class AsistenDosen extends Mahasiswa {
+  
+  String mataKuliah;
+
+  
+  AsistenDosen(String nama, String nim, String jurusan, int angkatan, this.mataKuliah)
+      : super(nama, nim, jurusan, angkatan);
+
+ 
+  @override
+  void tampilkanData() {
+    super.tampilkanData(); 
+    print('Mata Kuliah Asistensi : $mataKuliah');
+  }
+}
+
+
+void main() {
+  
+  var asdos = AsistenDosen(
+    'Aditya Ddarmawan',
+    '1125170115',
+    'Aplikasi Mobile',
+    2025,
+    'Pemrograman Dart',
+  );
+
+  
+  asdos.tampilkanData();
+}
